a9dd9f05a4a24cfa8a176769f536cd4cececfaf9
Bruin, Wouter de <wouter.de.bruin@capgemini.com>
2022-12-03 00:07:29 +0100

    Refactor day2 with dictionary for scoretable, modulo (not %) for wraparound to pick move.

--- Advent/Solutions/Day02a.cs
+++ Advent/Solutions/Day02a.cs
@@ -6,62 +6,26 @@ public class Day02a : DayBase
     {
         var lines = ReadInput("Day02.txt");
 
+        var dict = new Dictionary<string, int>
+        {
+            { "A X", 1+3 },
+            { "A Y", 2+6 },
+            { "A Z", 3+0 },
+
+            { "B X", 1+0 },
+            { "B Y", 2+3 },
+            { "B Z", 3+6 },
+
+            { "C X", 1+6 },
+            { "C Y", 2+0 },
+            { "C Z", 3+3 },
+        };
+        // De score voor elke situatie is index 1-3 en dan daarbij opgeteld 3,6,0(A)  0,3,6(B) 6,0,3(C)
+        
         int totalScore = 0;
         foreach (var round in lines)
         {
-            int score = 0;
-            var moves = round.Split(' ');
-
-            var opp = moves[0];
-            var me = moves[1];
-
-            switch(opp)
-            {
-                case "A":
-                    switch(me)
-                    {
-                        case "X":
-                            score += 1 + 3;
-                            break;
-                        case "Y":
-                            score += 2 + 6;
-                            break;
-                        case "Z":
-                            score += 3;
-                            break;
-                    }
-                break;
-                case "B":
-                    switch(me)
-                    {
-                        case "X":
-                            score += 1;
-                            break;
-                        case "Y":
-                            score += 2 + 3;
-                            break;
-                        case "Z":
-                            score += 3 + 6;
-                            break;
-                    }
-                break;
-                case "C":
-                    switch(me)
-                    {
-                        case "X":
-                            score += 1 + 6;
-                            break; 
-                        case "Y":
-                            score += 2;
-                        break;
-                        case "Z":
-                            score += 3 + 3;
-                        break;
-                    }
-                break;
-            }
-
-            totalScore += score;
+            totalScore += dict[round];           
         }
 
         WriteLine($"Total score: {totalScore}");
--- Advent/Solutions/Day02b.cs
+++ Advent/Solutions/Day02b.cs
@@ -1,4 +1,7 @@
-﻿namespace Advent.Solutions;
+﻿using Advent.Extensions;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Advent.Solutions;
 
 public class Day02b : DayBase
 {
@@ -6,79 +9,45 @@ public class Day02b : DayBase
     {
         var lines = ReadInput("Day02.txt");
 
-        int totalScore = 0;
-        foreach (var round in lines)
+        // De score voor elke situatie is index 1-3 en dan daarbij opgeteld 3,6,0(A)  0,3,6(B) 6,0,3(C) 
+        var dict = new Dictionary<string, int>
         {
-            int score = 0;
-            var moves = round.Split(' ');
+            { "A X", 1+3 },
+            { "A Y", 2+6 },
+            { "A Z", 3+0 },
 
-            var opp = moves[0];
-            var targetResult = moves[1];
+            { "B X", 1+0 },
+            { "B Y", 2+3 },
+            { "B Z", 3+6 },
 
-            string me = "";
-
-            switch(targetResult)
-            {
-                case "X":
-                    me = opp == "A" ? "Z" : opp == "B" ? "X" : "Y";
-                break;
-                case "Y":
-                    me = opp == "A" ? "X" : opp == "B" ? "Y" : "Z";
-                    break;
-                case "Z":
-                    me = opp == "A" ? "Y" : opp == "B" ? "Z" : "X";
-                    break;
-            }
-
-            switch(opp)
-            {
-                case "A":
-                    switch(me)
-                    {
-                        case "X":
-                            score += 1 + 3;
-                            break;
-                        case "Y":
-                            score += 2 + 6;
-                            break;
-                        case "Z":
-                            score += 3;
-                            break;
-                    }
-                break;
-                case "B":
-                    switch(me)
-                    {
-                        case "X":
-                            score += 1;
-                            break;
-                        case "Y":
-                            score += 2 + 3;
-                            break;
-                        case "Z":
-                            score += 3 + 6;
-                            break;
-                    }
-                break;
-                case "C":
-                    switch(me)
-                    {
-                        case "X":
-                            score += 1 + 6;
-                            break; 
-                        case "Y":
-                            score += 2;
-                        break;
-                        case "Z":
-                            score += 3 + 3;
-                        break;
-                    }
-                break;
-            }
-
-            totalScore += score;
+            { "C X", 1+6 },
+            { "C Y", 2+0 },
+            { "C Z", 3+3 },
+        };
+       
+        // x lose, y draw, z win
+        // To win, need index +1 or if at 3 % 3 to get to 0
+        // To draw, need same index
+        // To lose, need index -1 or if at -1 % 3 to get to 2
+        int totalScore = 0;
+        foreach (var tip in lines)
+        {
+            (string opp, Result res) = tip.Split(' ') switch { var a => (a[0], a[1] == "X" ? Result.Lose : a[1] == "Y" ? Result.Draw : Result.Win) };
+            var oppIdx = "ABC".IndexOf(opp);
+            var myIdx = res == Result.Lose ? oppIdx - 1 : res == Result.Draw ? oppIdx : oppIdx + 1;
+            myIdx = (myIdx < 0 || myIdx > 2) ? (myIdx.Mod(3)) : myIdx; // In C#, % is actually remainder, not modulo as expected! Got extension method.
+                                                                       // (-1 % 3 = -1, -1 mod 3 = 2)
+            string round = $"{opp} {"XYZ"[myIdx]}";
+            totalScore += dict[round];
         }
 
         WriteLine($"Total score: {totalScore}");
+    }
+
+    public enum Result
+    {
+        Lose,
+        Draw,
+        Win
     }
 }
