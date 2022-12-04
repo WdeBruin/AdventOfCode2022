636d6271aa084bbbd04aba68ae708f359e33d987
Bruin, Wouter de <wouter.de.bruin@capgemini.com>
2022-12-04 19:54:16 +0100

    Day04 done

---
+++ Advent/Solutions/Day04a.cs
@@ -0,0 +1,24 @@
+﻿namespace Advent.Solutions;
+
+public class Day04a : DayBase
+{
+    public override void Run()
+    {
+        var lines = ReadInput("Day04.txt");
+
+        int count = 0;
+        foreach(var pair in lines)
+        {
+            var one = pair.Split(',')[0];
+            var two = pair.Split(',')[1];
+
+            var oneN = one.Split('-').Select(int.Parse)?.ToArray();
+            var twoN = two.Split("-").Select(int.Parse)?.ToArray();
+
+            if ((oneN[0] <= twoN[0] && oneN[1] >= twoN[1]) || (twoN[0] <= oneN[0] && twoN[1] >= oneN[1]))
+                count++;
+        }                
+
+        WriteLine($"Total: {count}");
+    }
+}
---
+++ Advent/Solutions/Day04b.cs
@@ -0,0 +1,30 @@
+﻿namespace Advent.Solutions;
+
+public class Day04b : DayBase
+{
+    public override void Run()
+    {
+        var lines = ReadInput("Day04.txt");
+
+        int count = 0;
+        foreach(var pair in lines)
+        {
+            var one = pair.Split(',')[0];
+            var two = pair.Split(',')[1];
+
+            var oneN = one.Split('-').Select(int.Parse)?.ToArray();
+            var twoN = two.Split("-").Select(int.Parse)?.ToArray();
+
+            for (int i = oneN[0]; i <= oneN[1]; i++)
+            {
+                if (twoN[0] <= i && twoN[1] >= i)
+                {
+                    count++;
+                    break;
+                }
+            }
+        }                
+
+        WriteLine($"Total: {count}");
+    }
+}
