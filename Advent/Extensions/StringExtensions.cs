6bdd060d9c9411dcb327e44acca12523146fd8eb
Wouter de Bruin <wouter.de.bruin@capgemini.com>
2022-12-05 15:29:33 +0100

    Day 5 done.

--- Advent/Extensions/StringExtensions.cs
+++ Advent/Extensions/StringExtensions.cs
@@ -11,4 +11,14 @@ public static class StringExtensions
     {
         return val.Split(splitBy).ToIntArray();
     }
+    
+    public static IEnumerable<string> SplitInParts(this string s, int partLength) {
+        if (s == null)
+            throw new ArgumentNullException(nameof(s));
+        if (partLength <= 0)
+            throw new ArgumentException("Part length has to be positive.", nameof(partLength));
+
+        for (var i = 0; i < s.Length; i += partLength)
+            yield return s.Substring(i, Math.Min(partLength, s.Length - i)).Trim();
+    }
 }
---
+++ Advent/Solutions/Day05b.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Text.RegularExpressions;
+using Advent.Extensions;
+
+namespace Advent.Solutions;
+
+public class Day05b : DayBase
+{
+    public override void Run()
+    {
+        var turn = 0;
+        
+        var lines = ReadInput("Day05.txt").ToList();
+        
+        // stacks init
+        var stacks = new Stack<string>[9];
+        for (var i = 0; i < 9; i++)
+        {
+            stacks[i] = new Stack<string>();    
+        }
+
+        for (var i = 7; i >= 0; i--)
+        {
+            var chunks = lines[i].SplitInParts(4).ToArray();
+            for (var j = 0; j < chunks.Count(); j++)
+            {
+                if(!string.IsNullOrWhiteSpace(chunks[j]))
+                    stacks[j].Push(chunks[j]);
+            }
+        }
+
+        Log(turn, stacks);
+        turn++;
+
+        // movements
+        for (var i = 10; i < lines.Count; i++)
+        {
+           var move = Regex.Split(lines[i], @"\D+").Where(s => s != string.Empty).ToArray().ToIntArray();
+           if (move.Length != 3) throw new Exception("Move length not 3");
+           move[1]--;
+           move[2]--;
+
+           var batch = new string[move[0]];
+           for (var j = move[0]-1; j >= 0; j--)
+           {
+               batch[j] = stacks[move[1]].Pop();
+           }
+
+           foreach (var crate in batch)
+           {
+               stacks[move[2]].Push(crate);
+           }
+           
+           Log(turn, stacks);
+           turn++;
+        }
+    }
+
+    private static void Log(int turn, Stack<string>[] stacks)
+    {
+        WriteLine($"Turn {turn}");
+        stacks.Print();
+        WriteLine(string.Empty);
+    }
+}
