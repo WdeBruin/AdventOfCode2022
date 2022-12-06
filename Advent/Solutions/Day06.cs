0baa63d32822f379058e06f15ab7214423eb5a32
Wouter de Bruin <wouter.de.bruin@capgemini.com>
2022-12-06 15:01:59 +0100

    Refactor day 6

--- Advent/Solutions/Day06.cs
+++ Advent/Solutions/Day06.cs
@@ -8,20 +8,14 @@ public class Day06 : DayBase
     public override void Run()
     {
         var line = ReadInput("Day06.txt")[0];
+        var length = 4;
 
-        var unique = 0;
-        var lastChars = new List<char>();
-        for (int i = 0; i < line.Length; i++)
+        for (int i = length; i < line.Length; i++)
         {
-            if(lastChars.Count == 14)
-                lastChars.RemoveAt(0);
-            lastChars.Add(line[i]);
-
-            if (lastChars.Distinct().Count() == 14) // only difference for b and a is 14 instead of 4
-            {
-                WriteLine($"Answer: {i + 1}");
-                break;
-            }
+            var part = line[(i - length)..i];
+            if (part.Distinct().Count() != length) continue;
+            WriteLine($"Answer: {i}");
+            break;
         }
     }
 }
