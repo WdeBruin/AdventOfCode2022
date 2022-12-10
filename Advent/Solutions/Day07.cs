341b714827e772b5a51b26ac36bd8e9983acf76c
Bruin, Wouter de <wouter.de.bruin@capgemini.com>
2022-12-10 14:46:46 +0100

    Little start and test of day07

---
+++ Advent/Solutions/Day07.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Text.RegularExpressions;
+using Advent.Extensions;
+
+namespace Advent.Solutions;
+
+public class Day07 : DayBase
+{
+    public override void Run()
+    {
+        var lines = ReadInput("Day07.txt");
+
+        var solution = Solve(lines);
+        
+        WriteLine($"Answer: {solution}");
+    }
+
+    public string Solve(string[] lines)
+    {
+        var tree = new Collection<Node<FsObject>>();
+        Node<FsObject> activeNode = null;
+
+        foreach (var command in lines)
+        {
+            switch (command)
+            {
+                case "$ cd /":
+                    activeNode = CreateOrGetNode(tree, "/", true, activeNode);
+                    break;
+                case "ls":
+                    break;
+                case "$ cd ..":
+                    activeNode = activeNode.Parent ?? throw new Exception("Trying cd .. without parent");
+                    break;
+                case string c when c.StartsWith("cd"):
+                    var navigateTo = c.Split(' ').Last();
+                    activeNode = tree.First(x => x.Parent == activeNode && x.Value.DirName == navigateTo);
+                    break;
+                case string c when c.StartsWith("dir"):
+                    var dirName = c.Split(' ').Last().Trim();
+                    activeNode = CreateOrGetNode(tree, dirName, true, activeNode);
+                    break;
+                case string c when Regex.IsMatch(c, @"^\d+"):
+                    var size = c.Split(' ').First().Trim();
+                    var fileName = c.Split(' ').Last().Trim();
+                    activeNode = CreateOrGetNode(tree, fileName, false, activeNode, int.Parse(size));
+                    break;
+                default:
+                    throw new Exception("Command not recognized");
+            }
+        }
+
+        throw new NotImplementedException();
+    }
+
+    private Node<FsObject> CreateOrGetNode(Collection<Node<FsObject>> tree, string v, bool isDir, Node<FsObject> parent, int? size = null)
+    {
+        var node = tree.FirstOrDefault(x => x.Value.DirName == v || x.Value.FileName == v && x.Parent == parent);
+
+        if (node == null && isDir)
+        {
+            node = new Node<FsObject> { Value = new FsObject { DirName = v, IsDir = true }, Parent = parent };
+            tree.Add(node);
+        } 
+        else if (node == null && !isDir)
+        {
+            node = new Node<FsObject> { Value = new FsObject { FileName = v, IsDir = false }, Parent = parent };
+            tree.Add(node);
+        }
+
+        return node ?? throw new Exception("Somehow null node");
+    }
+
+    public class Node<T>
+    {
+        public Node<T>? Parent { get; set; }
+        public T Value { get; set; }
+    }
+
+    public class FsObject
+    {
+        public string? FileName { get; set; }
+        public string? DirName { get; set; }
+        public bool IsDir { get; set; }        
+    }
+}
---
+++ Tests/Day07Tests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+
+namespace Advent.Solutions;
+
+public class Day07Tests
+{
+    [Test]
+    public void Example()
+    {
+        var sut = new Day07();
+        var input = @"$ cd /
+                    $ ls
+                    dir a
+                    14848514 b.txt
+                    8504156 c.dat
+                    dir d
+                    $ cd a
+                    $ ls
+                    dir e
+                    29116 f
+                    2557 g
+                    62596 h.lst
+                    $ cd e
+                    $ ls
+                    584 i
+                    $ cd ..
+                    $ cd ..
+                    $ cd d
+                    $ ls
+                    4060174 j
+                    8033020 d.log
+                    5626152 d.ext
+                    7214296 k";
+        var lines = input.Split("\r\n");
+
+        var result = sut.Solve(lines);
+        Assert.AreEqual("95437", result);
+    }
+}
